25fd48ef7f5be433ab4c9da11d1e25356186f8c5
Larissa Valli <larissavalli4@gmail.com>
2025-08-26 16:29:19 -0300

    desafio concluido

---
+++ js-curso-2-desafio_1/js-curso-2-desafio_1/app.js
@@ -0,0 +1,35 @@
+//Desafio 1
+
+// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+
+// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+
+// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+
+// Ao clicar no botão soma, peça 2 números inteiros e exiba o resultado da soma em um alerta.
+
+
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function exibirMensagemNoConsole() {
+    console.log('O botão foi clicado!')
+}
+
+function exibirAlerta() {
+    alert('Eu amo Js')
+}
+
+function exibirPrompt() {
+    let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil que você gosta muito:')
+    alert(`Estive em ${nomeDaCidade} e lembrei de você`)
+}
+
+function somandoDoisNumeros() {
+    let primeiroNumero = parseInt(prompt('Digite o primeiro número'));
+    let segundoNumero = parseInt(prompt('Digite o segundo número'));
+    let resultado = primeiroNumero + segundoNumero;
+    alert(`${primeiroNumero} + ${segundoNumero} = ${resultado}`)
+}
